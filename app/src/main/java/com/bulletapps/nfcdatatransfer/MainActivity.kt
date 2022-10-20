0374c810dd4b46c3084e68a004b58071953a67e9
jvsena42 <jvsena16@gmail.com>
2022-10-20 15:40:40 -0300

    feat: create text Record WIP

--- app/src/main/java/com/bulletapps/nfcdatatransfer/MainActivity.kt
+++ app/src/main/java/com/bulletapps/nfcdatatransfer/MainActivity.kt
@@ -24,5 +24,23 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+    fun createTextRecord(payload: String, locale: Locale, encodeInUtf8: Boolean): NdefRecord {
+        val langBytes = locale.language.toByteArray(Charset.forName("US-ASCII"))
+        val utfEncoding = if (encodeInUtf8) Charset.forName("UTF-8") else Charset.forName("UTF-16")
+        val textBytes = payload.toByteArray(utfEncoding)
+        val utfBit: Int = if (encodeInUtf8) 0 else 1 shl 7
+        val status = (utfBit + langBytes.size).toChar()
+        val data = ByteArray(1 + langBytes.size + textBytes.size)
+        data[0] = status.toByte()
+        System.arraycopy(langBytes, 0, data, 1, langBytes.size)
+        System.arraycopy(textBytes, 0, data, 1 + langBytes.size, textBytes.size)
+        return NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, ByteArray(0), data)
+    }
 
+    fun createTextRecord(payload: String): NdefRecord {
+        return NdefRecord.createTextRecord(
+            "pt-BR", // TODO handle language code
+            payload
+        )
+    }
 }
