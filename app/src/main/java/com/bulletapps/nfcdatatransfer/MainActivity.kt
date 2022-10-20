1b6d0e20b86af72adf082e65c5db20d502e7135a
jvsena42 <jvsena16@gmail.com>
2022-10-20 15:34:58 -0300

    feat: getMessage intent

--- app/src/main/java/com/bulletapps/nfcdatatransfer/MainActivity.kt
+++ app/src/main/java/com/bulletapps/nfcdatatransfer/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.bulletapps.nfcdatatransfer
 
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
+import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.nio.charset.Charset
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getMessageIntent()
     }
+
+    private fun getMessageIntent() {
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
+            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMessages ->
+                val messages: List<NdefMessage> = rawMessages.map { it as NdefMessage }
+
+            }
+        }
+    }
+
 }
